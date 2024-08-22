ddd29ad80e95c4d9fda3c58251553adfde49ddde
villarescarmen <villarescarmenfp@gmail.com>
2024-08-22 09:30:57 +0200

    comenzqmos proyecto app

---
+++ calculadora.js
@@ -0,0 +1,16 @@
+window.onload = iniciar; 
+
+function iniciar (){
+    let btnCalcular = document.getElementById('btnCalcular');
+    btnCalcular.addEventListener('click',clickBtnCalcular);
+}
+
+function clickBtnCalcular(){
+    let txtPeso = document.getElementById('txtPeso');
+    let peso = txtPeso.value;
+    let txtAltura = document.getElementById('txtAltura');
+    let altura = txtAltura.value;
+    let imc = peso / (altura*altura);
+
+    alert("Tu IMC es " + Math.round(imc));
+}
